53333dfd4452d604c3f45e12b12515fedaf61dc7
Ferkka <92376072+Ferkka@users.noreply.github.com>
2022-12-29 11:48:06 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -16,8 +16,8 @@
 
 import os
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
-ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
-IP = str(os.environ.get("ip"))  # Xosting ip manzili
+
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+ADMINS = list(os.environ.get("ADMINS"))
+IP = str(os.environ.get("ip"))
 PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
